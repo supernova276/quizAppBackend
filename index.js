94847124f0947f32d7e2e1dcfa4f5cc1b682e483
Akshita <akshita.dixit2706@gmail.com>
2024-02-06 23:18:53 +0530

    added categories to index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(express.json())
 
 app.use(cors());
 
-app.get("/",(req,res)=>{
-   res.status(200).send({message:"hello world"})
+app.get("/categories",(req,res)=>{
+   res.status(200).json(quizzes)
 })
 
 quizzesRoutes(app)
